e6f1e149d900c0c12aa577620bc830165256e356
Martin Peshevski <martin.m.pesevski@gmail.com>
2020-03-22 10:40:26 -0400

    saving filters to user defaults

--- stocks/FilterView.swift
+++ stocks/FilterView.swift
@@ -21,6 +21,8 @@ class FilterView: UIView {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
         label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         label.lineBreakMode = .byWordWrapping
         label.numberOfLines = 0
         return label
@@ -30,6 +32,8 @@ class FilterView: UIView {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
         label.setContentHuggingPriority(.defaultLow, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         label.lineBreakMode = .byWordWrapping
         label.numberOfLines = 0
         return label
@@ -58,7 +62,7 @@ class FilterView: UIView {
     lazy var content: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [verticalStack, selectionBox])
         stack.axis = .horizontal
-        stack.distribution = .fillProportionally
+        stack.distribution = .fill
         stack.alignment = .center
         stack.spacing = 10
         stack.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
@@ -70,10 +74,14 @@ class FilterView: UIView {
         backgroundColor = .systemGray6
         title.text = filter.title
         explanation.text = filter.explanation
+        isSelected = UserDefaults.standard.bool(forKey: filter.title)
     }
     
     @objc func onTap() {
         isSelected = !isSelected
+        if let text = title.text {
+            UserDefaults.standard.set(isSelected, forKey: text)
+        }
     }
     
     required init?(coder: NSCoder) {
